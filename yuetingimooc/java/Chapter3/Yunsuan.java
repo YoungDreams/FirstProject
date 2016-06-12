3a4b3be09423a85920cae8964c7895d82bb1b6bb
YueTing <290061445@qq.com>
2016-06-12 15:47:06 +0800

    修改的

--- yuetingimooc/java/Chapter3/Yunsuan.java
+++ yuetingimooc/java/Chapter3/Yunsuan.java
@@ -1,13 +1,36 @@
 public class Yunsuan{
    public static void main(String[] args) {
-		int a=16;
-		double b=9.5;
-		String str1="hello";
-		String str2="imooc";
-		System.out.println("a等于b：" + (a ==  b));
-		System.out.println("a大于b：" + (a  > b));
-		System.out.println("a小于等于b：" + (a <=  b));
-		System.out.println("str1等于str2：" + (str1  == str2));
+		int num1=16;
+                int num2=1;
+                int num3=9;
+                int num4=15;
+                int num5=19;
+                int num6=8;
+                int num7=21;
+		double num8=9.5;
+                double num9=17.3;
+                double num10=15.6;
+                double num11=78.2;
+                double num12=45.1;
+                double num13=49.5;
+                double num14=23.2;
+		String one="hello";
+		String two="imooc";
+		System.out.println("num1等于num8：" + (num1 ==  num8));
+		System.out.println("num2大于num9：" + (num2  > num9));
+		System.out.println("num3小于等于num10：" + (num3 <=  num10));
+		System.out.println("num4不等于num11：" + (num4  ！= num11));
+                System.out.println("num5等于num12：" + (num5 == num12));
+		System.out.println("num6大于num13：" + (num6 > num13));
+		System.out.println("num7小于等于num14：" + (num7 <= num14));
+		System.out.println("num6等于num5：" + (num6  == num5));
+                System.out.println("num2等于num10：" + (num2 == num10);
+		System.out.println("num7大于num13：" + (num7  > num13));
+		System.out.println("num4小于等于num3：" + (num4 <=  num3));
+		System.out.println("one等于two：" + (one  == two));
+
+
+
 
 	}
 }
