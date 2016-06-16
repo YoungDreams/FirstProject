a254d9b4da134aec83aabe2c50d076b5dd19aabf
YueTing <290061445@qq.com>
2016-06-16 23:21:16 +0800

    Mrs.Li 亲启

---
+++ yuetingimooc/java/Chapter3/Logic operation.java
@@ -0,0 +1,16 @@
+public class Logic operation {
+    public static void main(String[] args) {
+		boolean a = true; // a同意
+		boolean b = false; // b反对
+		boolean c = false; // c反对
+		boolean d = true; // d同意
+	System.out.println((a&&b)+"未通过");
+        System.out.println((a丨丨)b+"通过"）;
+        System.out.println((!a)+"未通过”）;
+        System.out.println((a^b)+"通过"）;
+        System.out.println((a&&d)+"通过"）;
+    
+    
+    
+	}
+}
