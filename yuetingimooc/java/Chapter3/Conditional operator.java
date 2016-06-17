a85656bc824837d348ee428e92dbd6ddf98f5296
YueTing <290061445@qq.com>
2016-06-17 23:03:05 +0800

    Conditional operator

---
+++ yuetingimooc/java/Chapter3/Conditional operator.java
@@ -0,0 +1,30 @@
+public class Conditional operator{
+    public static void main(String[] args) {
+		int score=68;
+        int score1=70;
+        int score3=80;
+        int score4=62;
+        int score5=72;
+        int score6=71;
+        int score7=56;
+        int score8=49;
+        String mark =(score>=60)?"及格"："不及格"
+        String mark1 =(score1>=60)?"及格"："不及格"
+        String mark2 =(score2>=60)?"及格"："不及格"
+        String mark3 =(score3>=60)?"及格"："不及格"
+        String mark4 =(score4>=60)?"及格"："不及格"
+        String mark5 =(score5>=60)?"及格"："不及格"
+        String mark6 =(score6>=60)?"及格"："不及格"
+        String mark7 =(score7>=60)?"及格"："不及格"
+        String mark8 =(score8>=60)?"及格"："不及格"
+        System.out.println("考试成绩如何："+mark);
+        System.out.println("考试成绩如何："+mark1);
+        System.out.println("考试成绩如何："+mark2);
+        System.out.println("考试成绩如何："+mark3);
+        System.out.println("考试成绩如何："+mark4);
+        System.out.println("考试成绩如何："+mark5);
+        System.out.println("考试成绩如何："+mark6);
+        System.out.println("考试成绩如何："+mark7);
+        System.out.println("考试成绩如何："+mark8);
+	}
+}       
