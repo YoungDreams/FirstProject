64e7db5ff05f2ba4b0b28cfdd308a94cf4faa7fc
YueTing <290061445@qq.com>
2016-06-20 12:35:00 +0800

    Priortiy operation

---
+++ yuetingimooc/java/Chapter3/Priortiy operation.java
@@ -0,0 +1,15 @@
+public class Priortiy operation {
+    public static void main(String[] args) {
+        int a = 6;
+		int m = 5;
+		int n = 7;
+		int x = (m*8/(n+2))%m;
+        int z = (11+3*8)/4%3;
+        int f = (7*(a+5)+12)/3%5;
+		System.out.println("m:" + m);
+		System.out.println("n:" + n);
+		System.out.println("x:" + x);
+        System.out.println("z:" + z);
+        System.out.println("f:" + f);
+	}
+}
