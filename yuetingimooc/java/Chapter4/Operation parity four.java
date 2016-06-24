6051925c9a41e0bd64a12a85b0b983a78644cce5
YueTing <290061445@qq.com>
2016-06-24 17:02:52 +0800

    four

---
+++ yuetingimooc/java/Chapter4/Operation parity four.java
@@ -0,0 +1,27 @@
+public class Operation parity four{
+    public static void main(String[] args) {
+		int score = 94;
+		String sex = "女";
+        if (score>80){
+        if (sex=="女"){
+        System.out.println("进入女子组决赛");   
+            }
+        else {
+        System.out.println("进入男子组决赛");
+            }
+        
+        }
+        String today="周末";
+        String weather="晴天";
+          if(today.equals("周末")){
+              if (weather.equals("晴天")){
+                  System.out.println("去室外游乐场玩");
+                  }
+                else{
+                  System.out.println("去室内游乐场玩");
+                  }
+                  
+     }
+    }
+        
+	}
