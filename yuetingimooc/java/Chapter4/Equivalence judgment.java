8c3cbb6c45cb57bc15c53834909ed9dc686d9c89
YueTing <290061445@qq.com>
2016-06-24 20:56:25 +0800

    怎么吧三个都输出来！！

---
+++ yuetingimooc/java/Chapter4/Equivalence judgment.java
@@ -0,0 +1,19 @@
+public class Equivalence judgment {
+    public static void main(String[] args) {
+		char today='日'; 
+		switch(today){
+    	 case 1:
+         case 3:  
+         case 5:
+             System.out.println("吃包子");
+         case 2:
+         case 4:
+         case 6:
+             System.out.println("吃油条");
+         default :
+             System.out.println("吃主席套餐");
+		}
+        
+ 
+	}
+}
