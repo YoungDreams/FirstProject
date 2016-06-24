005471055aa43e5323945e62f5f8cfb67d4469c1
YueTing <290061445@qq.com>
2016-06-24 13:39:58 +0800

    Operation parity three

---
+++ yuetingimooc/java/Chapter4/Operation parity three.java
@@ -0,0 +1,23 @@
+public class Operation parity three{
+    public static void main(String[] args) {
+		    int size=15;
+            if(size>=13){
+            System.out.println("奖励一部苹果6");
+        }   else if (size<11){
+            System.out.println("奖励一瓶水");   
+        }
+            else{
+            System.out.println("就把菊花送给他");
+            }
+            int age=27;
+            if(age>23){
+            System.out.println("有助学金");
+            }
+            else if(age<=25){
+            System.out.println("有助学金");
+            }
+            else{
+            System.out.println("没有助学金");
+            }
+             }
+}
