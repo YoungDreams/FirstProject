387eaca1c53b9db09b714e57cb4fe32c285d025b
YueTing <290061445@qq.com>
2016-06-24 12:47:54 +0800

    李振和菊花不为人知的秘密

---
+++ yuetingimooc/java/Chapter4/Operation parity two.java
@@ -0,0 +1,17 @@
+public class Operation parity two {
+    public static void main(String[] args) {
+		int age=25;
+		if(age>=20){
+    	 System.out.println("每天晚上可以干李振十次");
+         }
+         else{
+         System.out.println("每天晚上可以干菊花十次") ;       
+		}
+        if(age<=23){
+        System.out.println("每天晚上可以李振花十次") ;    
+         }
+         else{
+         System.out.println("每天晚上可以干菊花十次") ;         
+        }
+	}
+}
