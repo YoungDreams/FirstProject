7086ce57b21fb4519bff374177ee99d2e25864dd
YueTing <290061445@qq.com>
2016-06-30 21:34:56 +0800

    Ask for advice

---
+++ yuetingimooc/java/Chapter4-6/Loop statement three.java
@@ -0,0 +1,21 @@
+public class Loop statement three {
+    public static void main(String[] args) {
+       for(int i=1, j=5; i<=5; i++,j--){
+       System.out.println(i+"+"+j+"="+(i+j)); 
+    }
+       for(int i=1;i<10 && i!=5; i++){
+       System.out.println(i);    
+    }
+       int sum=0;
+       for(int i=1;i<=100;i++){
+       if(i%3!=0){
+        sum=sum+i;
+       } 
+    }
+    System.out.println("1到100之间不能被3整除的数之和为：" + sum);
+    int i =0;
+    for(;i<10;i++) {
+    System.out.println("李振，祝福你和菊花百年好合"); }
+    }
+
+	}
