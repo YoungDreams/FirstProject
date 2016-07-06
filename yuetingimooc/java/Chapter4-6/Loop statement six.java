c9bf65f84a42a5b8aa8c96e8137d8cca00c87e2f
YueTing <290061445@qq.com>
2016-07-06 14:02:12 +0800

    Ask for advice

---
+++ yuetingimooc/java/Chapter4-6/Loop statement six.java
@@ -0,0 +1,23 @@
+public class Loop statement six {
+    public static void main(String[] args) {
+
+	    int sum = 0; // 保存累加值
+	    for (int i = 1; i <= 10; i++) {
+	    if (i%2 ==0) {
+            sum=sum+i;
+			}  
+            continue;
+              }
+	    System.out.print("1到10之间的所有偶数的和为：" + sum);
+        
+            int number= 0 ;
+            for(int i=1;i<=15;i++){
+            if(i%3 !=0){
+            number=number+i;
+        }
+            continue;
+    }
+	    System.out.print("1到10之间的所有奇数的和为：" + number);
+
+	}
+}
